adbe949b54e4e9ba546ddc8d8dd04bec8b1a414a
nick <nick.zwart@gmail.com>
2015-10-10 15:33:52 -0700

    Added node wrapping caldir. -untested

--- gpi/CalDir_GPI.py
+++ gpi/CalDir_GPI.py
@@ -18,36 +18,32 @@ base_path = bart.__path__[0] # library base for executables
 import bart.python.cfl as cfl
 
 class ExternalNode(gpi.NodeAPI):
-    '''Usage: calmat [-k kernel_size] [-r cal_size] <kspace> <calibration matrix>
-    
-    Compute calibration matrix.
-    
-    -k ksize    kernel size
-    -r cal_size Limits the size of the calibration region.
+    '''Usage: ./caldir cal_size <input> <output>
+
+        Estimates coil sensitivities from the k-space center using
+        a direct method (McKenzie et al.). The size of the fully-sampled
+        calibration region is automatically determined but limited by
+        {cal_size} (e.g. in the readout direction).
     '''
 
     def initUI(self):
         # Widgets
-        self.addWidget('SpinBox', 'kernel size', min=1, val=6)
         self.addWidget('SpinBox', 'calibration region size', min=1, val=20)
 
         # IO Ports
-        self.addInPort('kspace', 'NPYarray', obligation=gpi.REQUIRED)
+        self.addInPort('input', 'NPYarray', obligation=gpi.REQUIRED)
         self.addOutPort('output', 'NPYarray')
 
         return 0
 
     def compute(self):
 
-        k = self.getVal('kernel size')
         r = self.getVal('calibration region size')
-
-        kspace = self.getData('kspace')
+        kspace = self.getData('input')
 
         # load up arguments list
-        args = [base_path+'/calmat']
+        args = [base_path+'/caldir']
         args += ['-r '+str(r)]
-        args += ['-k '+str(k)]
 
         # setup file for passing data to external command
         in1 = IFilePath(cfl.writecfl, kspace, asuffix=['.cfl','.hdr'])
