141173593d219cdc53fc0724932692ead045ebab
ryan <rkrobison@gmail.com>
2015-10-10 00:13:33 -0700

    Roughed out cc and ecalib functions

---
+++ gpi/CoilCompression_GPI.py
@@ -0,0 +1,85 @@
+# GPI (v0.5.0-n1) auto-generated library file.
+#
+# FILE: /Users/nick/gpi/nick/default/GPI/MyNode_GPI.py
+#
+# For node API examples (i.e. widgets and ports) look at the
+# core.interfaces.Template node.
+
+import os
+
+# gpi, future
+import gpi
+from bart.python.ebe import IFile, OFile, Command
+
+# bart
+import bart
+base_path = bart.__path__[0] # library base for executables
+import bart.python.cfl as cfl
+
+class ExternalNode(gpi.NodeAPI):
+    '''Usage: cc [-A] [-r cal_size] [-P num_coeffs] <kspace> <coeff>|<proj_kspace>
+    
+    Performs coil compression.
+    
+    -P N    perform compression to N virtual channels
+    -r S    size of calibration region
+    -A  use all data to compute coefficients
+    -S|G|E  type: SVD, Geometric, ESPIRiT
+    -h  help
+    '''
+
+    def initUI(self):
+        # Widgets
+        self.addWidget('SpinBox', 'virtual channels', min=1, val=5)
+        self.addWidget('SpinBox', 'calibration size', min=1, val=10)
+        self.addWidget('PushButton', 'Use All Data', toggle=True)
+        self.addWidget('ExclusivePushButtons', 'Compression Type:', buttons=['SVD', 'Geometric', 'ESPIRiT'], val=0)
+
+        # IO Ports
+        self.addInPort('kspace', 'NPYarray', obligation=gpi.REQUIRED)
+        self.addInPort('coeff', 'NPYarray', obligation=gpi.REQUIRED)
+        self.addOutPort('proj_kspace', 'NPYarray')
+
+        return 0
+
+    def compute(self):
+
+        p = self.getVal('virtual channels')
+        r = self.getVal('calibration size')
+        a = self.getVal('Use All Data')
+        comptype = self.getVal('Compression Type:')
+
+        kspace = self.getData('kspace')
+        coeff = self.getData('coeff')
+
+        # load up arguments list
+        args = [base_path+'/cc']
+        args += ['-P '+str(p)]
+        args += ['-r '+str(r)]
+        if a:
+            args += ['-A']
+
+        if comptype == 0:
+            args += ['-S']
+        elif comptype == 1:
+            args += ['-G']
+        else:
+            args += ['-E']
+
+        # setup file for passing data to external command
+        in1 = IFile(cfl.writecfl, kspace, asuffix=['.cfl','.hdr'])
+        args += [in1]
+        in2 = IFile(cfl.writecfl, coeff, asuffix=['.cfl','.hdr'])
+        args += [in2]
+
+        # setup file for getting data from external command
+        out = OFile(cfl.readcfl, asuffix=['.cfl','.hdr'])
+        args += [out]
+
+        # run commandline
+        print Command(args)
+
+        self.setData('proj_kspace', out.data())
+        out.close()
+
+        return 0
---
+++ gpi/ECalib_GPI.py
@@ -0,0 +1,93 @@
+# GPI (v0.5.0-n1) auto-generated library file.
+#
+# FILE: /Users/nick/gpi/nick/default/GPI/MyNode_GPI.py
+#
+# For node API examples (i.e. widgets and ports) look at the
+# core.interfaces.Template node.
+
+import os
+
+# gpi, future
+import gpi
+from bart.python.ebe import IFile, OFile, Command
+
+# bart
+import bart
+base_path = bart.__path__[0] # library base for executables
+import bart.python.cfl as cfl
+
+class ExternalNode(gpi.NodeAPI):
+    '''Usage: ./ecalib [-n num. s.values] [-t eigenv. threshold] [-c crop_value] [-k kernel_size] [-r cal_size] [-m maps] <kspace> <sensitivites> [<ev-maps>]
+    
+    Estimate coil sensitivities using ESPIRiT calibration.
+    Optionally outputs the eigenvalue maps.
+    
+    -t threshold    This determined the size of the null-space.
+    -c crop_value   Crop the sensitivities if the eigenvalue is smaller than {crop_value}.
+    -k ksize    kernel size
+    -r cal_size Limits the size of the calibration region.
+    -m maps     Number of maps to compute.
+    -I      intensity correction
+    -1      perform only first part of the calibration
+    '''
+
+    def initUI(self):
+        # Widgets
+        self.addWidget('DoubleSpinBox', 'threshold', min=.01, val=1.0)
+        self.addWidget('DoubleSpinBox', 'crop value', min=.01, val=1.0)
+        self.addWidget('SpinBox', 'kernel size', min=1, val=5)
+        self.addWidget('SpinBox', 'calibration size', min=1, val=20)
+        self.addWidget('SpinBox', 'number maps', min=1, val=2)
+        self.addWidget('PushButton', 'intensity correction', toggle=True)
+        self.addWidget('PushButton', '1st part only', toggle=True)
+
+        # IO Ports
+        self.addInPort('kspace', 'NPYarray', obligation=gpi.REQUIRED)
+        self.addOutPort('sensitivities', 'NPYarray')
+        self.addOutPort('ev_maps', 'NPYarray')
+
+        return 0
+
+    def compute(self):
+
+        t = self.getVal('threshold')
+        c = self.getVal('crop value')
+        k = self.getVal('kernel size')
+        r = self.getVal('calibration size')
+        m = self.getVal('number maps')
+        I = self.getVal('intensity correction')
+        first = self.getVal('1st part only')
+
+        kspace = self.getData('kspace')
+
+        # load up arguments list
+        args = [base_path+'/ecalib']
+        args += ['-t '+str(t)]
+        args += ['-c '+str(c)]
+        args += ['-k '+str(k)]
+        args += ['-r '+str(r)]
+        args += ['-m '+str(m)]
+        if I:
+            args += ['-I']
+        if first:
+            args += ['-1']
+
+        # setup file for passing data to external command
+        in1 = IFile(cfl.writecfl, kspace, asuffix=['.cfl','.hdr'])
+        args += [in1]
+
+        # setup file for getting data from external command
+        out1 = OFile(cfl.readcfl, asuffix=['.cfl','.hdr'])
+        args += [out1]
+        out2 = OFile(cfl.readcfl, asuffix=['.cfl','.hdr'])
+        args += [out2]
+
+        # run commandline
+        print Command(args)
+
+        self.setData('sensitivities', out1.data())
+        out1.close()
+        self.setData('ev_maps', out2.data())
+        out2.close()
+
+        return 0
